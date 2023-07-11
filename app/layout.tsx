7c6961346ae9b76658c01a0ab04bc6eb66fd5663
lucasweidas <lucasweidas@gmail.com>
2023-07-11 16:39:21 -0300

    add starter styles config

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
 
-const roboto = Roboto({ subsets: ['latin'], weight: ['400', '700'] });
+const roboto = Roboto({ subsets: ['latin'], weight: ['400', '700'], variable: '--font-roboto' });
 
 export const metadata: Metadata = {
   title: 'Frontend Mentor | Newsletter sign-up',
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={roboto.className}>{children}</body>
+      <body className={`${roboto.variable} font-sans`}>{children}</body>
     </html>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ['var(--font-roboto)'],
+      },
+      colors: {
+        tomato: {
+          400: 'hsl(4, 100%, 67%)',
+        },
+        grey: {
+          400: 'hsl(231, 7%, 60%)',
+          700: 'hsl(235, 18%, 26%)',
+          800: 'hsl(234, 29%, 20%)',
+        },
       },
     },
   },
   plugins: [],
-}
+};
